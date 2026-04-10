7bf1a4eb571ac1f3290e3dd854553862f4e2ce4b
mishi54 <mishi121006@gmail.com>
2026-04-09 23:13:00 -0600

    added schema for product

--- backend/schema/productSchema.js
+++ backend/schema/productSchema.js
@@ -1,1 +1,11 @@
 import Joi from "joi";
+
+export const productSchema = Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().required(),
+    image:Joi.array().items(Joi.string()).required(),
+    category: Joi.string().required(),
+    stock: Joi.number().required(),
+    rating: Joi.number().default(0),
+})
